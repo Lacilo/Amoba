ea355fbf110ac8e7dde5f7b166bcad40e06ef450
Lacilo <szaszfail@kkszki.hu>
2025-09-25 13:07:22 +0200

    Kezdeti megjelenítés függvények és a mátrixot generáló függvény létrehozása

--- Amoba/Program.cs
+++ Amoba/Program.cs
@@ -6,8 +6,8 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Text
-using System.Globalization
+using System.Text;
+using System.Globalization;
 
 namespace Amoba
 {
@@ -15,8 +15,46 @@ namespace Amoba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
-    }
+
+        // KEZDETI (TESZT) MÁTRIXOT GENERÁLÓ FÜGGVÉNY
+        /// <summary>
+        /// Ez a függvény egy mátrixot generál
+        /// </summary>
+        /// <returns></returns>
+        static char[,] MatrixGenerate() 
+        {
+            char[,] matrix = new char[10, 10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    matrix[i, j] = char.Parse("x");
+                }
+            }
+
+            return matrix;
+        }
+
+        // MÁTRIXOT MEGJELENÍTŐ ELJÁRÁS
+        /// <summary>
+        /// Ez a függvény megjeleníti a mátrixot
+        /// </summary>
+        /// <param name="matrix"></param>
+        static void DisplayMatrix(char[,] matrix)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(matrix[i, j] + " | ");
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+    }    
 }
 
