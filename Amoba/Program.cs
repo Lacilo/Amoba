7b9c0f0da61da121adecdd02ef0214e51d218aae
Zsombor <szilagyizs@kkszki.hu>
2025-09-25 12:57:32 +0200

    Alap menu létrehozása

--- Amoba/Program.cs
+++ Amoba/Program.cs
@@ -15,7 +15,95 @@ namespace Amoba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int aktualisPont = 0;
+            do
+            {
+                bool select = false;
+                do
+                {
+                    ShowMenu(aktualisPont);
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.Enter:
+                            select = true;
+                            break;
+                        case ConsoleKey.UpArrow:
+                            if (aktualisPont > 0)
+                            {
+                                aktualisPont--;
+                            }
+                            break;
+                        case ConsoleKey.DownArrow:
+                            if (aktualisPont < 1)
+                            {
+                                aktualisPont++;
+                            }
+                            break;
+                        default:
+                            Console.Beep();
+                            break;
+                    }
+
+                }
+                while (!select);
+                switch (aktualisPont)
+                {
+                    case 0: //Új játék
+                        
+                        break;          
+                    case 1: //Kilépés
+                        Console.Clear();
+                        Console.SetCursorPosition(50, 0);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Amőba 1.0 Menu");
+                        Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+                        Console.Write("Biztosan kilépsz?");
+                        if (Console.ReadKey().Key != ConsoleKey.Enter)
+                        {
+                            aktualisPont = 0;
+                        }
+                        break;
+
+
+                }
+            }
+            while (aktualisPont != 1);
+        }
+
+        static void ShowMenu(int cPoint)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(50, 0);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Amőba 1.0 Menu");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+            
+            if (cPoint == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("1 - Új játék");
+            if (cPoint == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("2 - Kilépés");
+            if (cPoint == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+            }
         }
     }
 }
