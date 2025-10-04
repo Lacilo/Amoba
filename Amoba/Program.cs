5bb134aea24c36fe5267ced3af50fb1e9dffad0b
Zsomborkkszki <szilagyizs@kkszki.hu>
2025-10-04 18:10:26 +0200

    nem mukodik

--- Amoba/Program.cs
+++ Amoba/Program.cs
@@ -11,11 +11,86 @@ using System.Globalization;
 
 namespace Amoba
 {
+    // FŐ PROGRAM
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            int mSize = 10;
+            int[] pos = { 0, 0 };
+            string[,] matrix = MatrixGenerate(mSize);
+            string uInput = "";
+            int[] uPos = new int[2];
+            int[] vPos = new int[2];
+            string symbol = "x";
+            bool validPos = true;
+
+            Menu();
+            DisplayMatrix(matrix, pos);
+
+            do
+            {
+                Console.Write("Adja meg a pozíciót --> ");
+                uInput = Console.ReadLine();
+
+                if (uInput != "")
+                {
+                    uPos[0] = int.Parse(uInput.Split(' ')[0]) - 1;
+                    uPos[1] = int.Parse(uInput.Split(' ')[1]) - 1;
+
+                    if (validPos)
+                    {
+                        matrix = MatrixAppend(matrix, uPos[1], uPos[0], symbol);
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else
+                {
+                    do
+                    {
+                        pos = ChangePos(pos, mSize);
+
+                        if (pos[0] != -1)
+                        {
+                            vPos[0] = pos[0];
+                            vPos[1] = pos[1];
+                        }
+
+                        DisplayMatrix(matrix, vPos);
+                    }
+                    while (pos[0] != -1);
+
+                    Console.WriteLine($"{vPos[0]} {vPos[1]}");
+
+                    if (validPos)
+                    {
+                        matrix = MatrixAppend(matrix, vPos[0], vPos[1], symbol);
+                    }
+                    else
+                    {
+
+                    }
+                }
+
+                DisplayMatrix(matrix, vPos);
+
+                pos[0] = vPos[0];
+                pos[1] = vPos[1];
+
+                if (symbol == "x" && validPos)
+                {
+                    symbol = "o";
+                }
+                else
+                {
+                    symbol = "x";
+                }
+
+            }
+            while (true);
         }
 
         private static void Menu()
@@ -67,12 +142,64 @@ namespace Amoba
                         {
                             aktualisPont = 0;
                         }
+                        Environment.Exit(0);
                         break;
 
 
                 }
             }
-            while (aktualisPont != 1);
+            while (aktualisPont != 0);
+        }
+
+        static int[] ChangePos(int[] pos, int mSize)
+        {
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.UpArrow:
+                    pos[1]--;
+
+                    break;
+
+                case ConsoleKey.RightArrow:
+                    pos[0]++;
+
+                    break;
+
+                case ConsoleKey.LeftArrow:
+                    pos[0]--;
+                    break;
+
+                case ConsoleKey.DownArrow:
+                    pos[1]++;
+                    break;
+
+                case ConsoleKey.Enter:
+                    pos[0] = -1;
+                    pos[1] = -1;
+                    break;
+
+                default:
+                    break;
+            }
+
+            return pos;
+        }
+
+        static string[,] MatrixAppend(string[,] matrix, int pos_y, int pos_x, string character)
+        {
+            // ITT HÍVNÁM MEG A FÜGGVÉNYT AMI ELLENŐRZI HOGY JÓ HELYRE TESZI-E ENNEK EGY BOOLEANT KELL VISSZAADNIA
+            bool correctPlace = true; // FüggvényedNeve(pos_x, pos_y);
+
+            if (correctPlace)
+            {
+                matrix[pos_x, pos_y] = character;
+            }
+            else
+            {
+
+            }
+
+            return matrix;
         }
 
         static void ShowMenu(int cPoint)
@@ -116,15 +243,15 @@ namespace Amoba
         /// Ez a függvény egy mátrixot generál
         /// </summary>
         /// <returns></returns>
-        static char[,] MatrixGenerate()
+        static string[,] MatrixGenerate(int mSize = 10)
         {
-            char[,] matrix = new char[10, 10];
+            string[,] matrix = new string[mSize, mSize];
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < mSize; i++)
             {
-                for (int j = 0; j < 10; j++)
+                for (int j = 0; j < mSize; j++)
                 {
-                    matrix[i, j] = char.Parse("x");
+                    matrix[i, j] = " ";
                 }
             }
 
@@ -136,19 +263,68 @@ namespace Amoba
         /// Ez a függvény megjeleníti a mátrixot
         /// </summary>
         /// <param name="matrix"></param>
-        static void DisplayMatrix(char[,] matrix)
+        static void DisplayMatrix(object[,] matrix, int[] pos)
         {
-            for (int i = 0; i < 10; i++)
+            Console.Clear();
+
+            int mDSize = matrix.GetLength(0);
+
+            // FELSŐ SÁV KIRAJZOLÁSA
+            Console.Write("┌─");
+
+            for (int i = 0; i < mDSize - 1; i++)
             {
-                for (int j = 0; j < 10; j++)
+                Console.Write("──┬─");
+            }
+
+            Console.Write("──┐\n");
+
+
+            // MÁTRIX KIRAJZOLÁSA
+            for (int i = 0; i < mDSize; i++)
+            {
+                Console.Write("│ ");
+                for (int j = 0; j < mDSize; j++)
                 {
-                    Console.Write(matrix[i, j] + " | ");
+                    if (i == pos[1] && j == pos[0])
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+
+                    Console.Write(matrix[i, j]);
+
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    Console.Write(" │ ");
                 }
 
-                Console.WriteLine("\n");
+                // HA MÉG NEM AZ UTOLSÓ SORNÁL JÁR
+                if (i != mDSize - 1)
+                {
+                    Console.Write("\n├─");
+
+                    for (int x = 0; x < mDSize - 1; x++)
+                    {
+                        Console.Write("──┼─");
+                    }
+
+                    Console.WriteLine("──┤");
+                }
             }
+
+
+            // ALSÓ SÁV KIRAJZOLÁSA
+            Console.Write("\n└─");
+
+            for (int i = 0; i < mDSize - 1; i++)
+            {
+                Console.Write("──┴─");
+            }
+
+            Console.Write("──┘\n");
         }
-        
+
     }
 }
-
